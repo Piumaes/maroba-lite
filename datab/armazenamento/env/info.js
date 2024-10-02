f2dcfca49a8c34a883bd5b3c2fa7230a7e13a352
Piumaes <utv1521@gmail.com>
2024-10-02 16:01:27 -0300

    first commit

---
+++ datab/armazenamento/env/info.js
@@ -0,0 +1,10 @@
+// Configurações & Informações
+
+const configurações = {
+  prefixo: "/",
+  nomeBot: "Luciano Store", // Nome do bot
+  nomeDono: "Loja Do Luciano", // Seu nome
+  numeroDono: "5528999501464" // Seu número
+}
+
+global.configurações = configurações // Ton >|<
